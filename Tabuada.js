d6d045dd06ba0e0426271c7fe2fe221a890babea
Marcos Akihito Sato <114444361+mrcosato@users.noreply.github.com>
2023-01-28 10:17:38 -0300

    Add files via upload

---
+++ Tabuada.js
@@ -0,0 +1,32 @@
+//Programa que exibe a tabuada do numero 2
+//armazenado na variavel de nome n
+var readlineSync = require('readline-sync');
+//arnazebanebti ba varuavek de bine b
+//var n = parseInt(readlineSync.question('1 valor: '));
+console.log("programa que exibe a tabuada de um numero");
+var n = parseInt(readlineSync.question("Informe um numero: "));
+console.log("Tabuada do numero " + n);
+console.log(n+ " x 0 = " + (n*0));
+console.log(n+ " x 1 = " + (n*1));
+console.log(n+ " x 2 = " + (n*2));
+console.log(n+ " x 3 = " + (n*3));
+console.log(n+ " x 4 = " + (n*4));
+console.log(n+ " x 5 = " + (n*5));
+console.log(n+ " x 6 = " + (n*6));
+console.log(n+ " x 7 = " + (n*7));
+console.log(n+ " x 8 = " + (n*8));
+console.log(n+ " x 9 = " + (n*9));
+console.log(n+ " x 10 = " + (n*10));
+var n = 0;
+
+/*n = parseInt(readlineSync.question( n + " x 0 = " + (n*0)));
+n = parseFInt(readlineSync.question(n+ " x 1 = " + (n*1)));
+n = parseInt(readlineSync.question(n+ " x 2 = " + (n*2)));
+n = parseInt(readlineSync.question(n+ " x 3 = " + (n*3)));
+n = parseInt(readlineSync.question(n+ " x 4 = " + (n*4)));
+n = parseInt(readlineSync.question(n+ " x 5 = " + (n*5)));
+n = parseInt(readlineSync.question(n+ " x 7 = " + (n*7)));
+n = parseInt(readlineSync.question(n+ " x 8 = " + (n*8)));
+n = parseInt(readlineSync.question(n+ " x 9 = " + (n*9)));
+n = parseInt(readlineSync.question(n+ " x 10 = " + (n*10))); 
+*/
